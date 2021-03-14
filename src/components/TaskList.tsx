1cd6cceff4c77e472a98caee0aedb7dbf5990607
Rubens Back <rubensback@gmail.com>
2021-03-14 18:24:05 +0000

    finish challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+
+    const newTask:Task = {
+      id: Math.random() * 100,
+      isComplete: false,
+      title: newTaskTitle,
+    }
+
+    setNewTaskTitle('')
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskToEditIndex = tasks.findIndex(task => task.id === id)
+
+    const tasksList = tasks
+
+    tasksList[taskToEditIndex] = {
+      ...tasksList[taskToEditIndex],
+      isComplete: !tasksList[taskToEditIndex].isComplete
+    }
+
+    setTasks([...tasksList])
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
